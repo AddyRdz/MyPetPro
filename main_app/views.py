beb17528752692f97c5601b998d7fc18d9769a51
Addy Rodriguez <rdz.33.a@gmail.com>
2022-03-08 18:53:02 -0600

    set up home and about

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.views import View
+from django.views.generic.base import TemplateView
+from django.http import HttpResponse
 # Create your views here.
+
+class Home(TemplateView):
+    # def get(self, request):
+    #     return HttpResponse("MyPetPro Home")
+    template_name="home.html"
+
+class About(TemplateView):
+    template_name="about.html"
+    # def get(self,request):
+    #     return HttpResponse("MyPetPro About")
+    
