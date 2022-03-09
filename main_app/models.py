89c755b27cbe95c52d210179515ca85399dff6c9
Addy Rodriguez <rdz.33.a@gmail.com>
2022-03-09 11:30:58 -0600

    Set up SuperUser

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Pet(models.Model):
 
@@ -7,8 +8,7 @@ class Pet(models.Model):
     img = models.CharField(max_length=250)
     owner = models.CharField(max_length=100)
     bio = models.TextField(max_length=500)
-    # verified_artist = models.BooleanField(default=False)
-    # created_at = models.DateTimeField(auto_now_add=True)
+    
 
     def __str__(self):
         return self.name
