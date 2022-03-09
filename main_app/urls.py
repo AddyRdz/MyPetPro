df58a7081f30ad838305635dbdf3ac3e475d92c4
Addy Rodriguez <rdz.33.a@gmail.com>
2022-03-08 23:20:53 -0600

    Set up pet_list route

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
     path('about/', views.About.as_view(), name="about"),
+    path('pet/', views.PetList.as_view(), name="pet_list")
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,23 @@
+from unicodedata import name
 from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
 from django.http import HttpResponse
 # Create your views here.
+
+# Static Data
+class Pet:
+    def __init__(self, name, image, owner, bio):
+        self.name=name
+        self.image=image
+        self.owner=owner
+        self.bio=bio
+
+# Seed Data
+pets = [
+    Pet("Joe", "img", "George Washington", "Joe loves going on walks and enjoys eating his dinner every night at 6 o clock."),
+    Pet("Sprinkles", "img", "Lisa Siri", "Sprinkles is your friendly neighborhood cat and would rather be left outside than be stuck inside all day.")
+]
 
 class Home(TemplateView):
     # def get(self, request):
@@ -14,3 +29,10 @@ class About(TemplateView):
     # def get(self,request):
     #     return HttpResponse("MyPetPro About")
     
+class PetList(TemplateView):
+    template_name="pet_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['pet'] = pets
+        return context
